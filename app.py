a61360be352709d4df15997fc6bbcae7e7925149
Tanuithemenace <Tanuilelan@gmail.com>
2023-06-27 18:12:10 +0300

    New libraries

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from os import environ
+from dotenv import load_dotenv
 
-
+#Initialize app
 app = Flask(__name__)
+#Secret Key
+SECRET_KEY = environ.get('SECRET_KEY')
 
 @app.route('/<name>')
 def home(name):
-    return f'This is the home page. Hello {name}'
+    return f'This is the home page. Hello {name} {SECRET_KEY}'
+
+@app.route('/home')
+def index():
+    return render_template('index.html',title='This is the homepage')
 
